71ec70f7ff3f007ff1472b721c6264407df7ae67
anasahmed-10p <anas.ahmed@10pearls.com>
2021-07-13 07:54:31 -0400

    first commit

---
+++ src/main/java/com/stacksmplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,14 @@
+package com.stacksmplify.restservices.springbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+}
